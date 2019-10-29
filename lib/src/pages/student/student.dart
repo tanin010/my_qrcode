7514a6a48e7851ad11d8e5408130ce36931937c7
tanin010 <53853461+tanin010@users.noreply.github.com>
2019-10-29 18:18:41 +0700

    ghj

--- lib/src/pages/student/student.dart
+++ lib/src/pages/student/student.dart
@@ -1,108 +1,84 @@
-import 'package:flutter/material.dart';         
+import 'package:flutter/material.dart';
+import 'package:my_qrcode/src/models/subjects.dart';
 import 'package:my_qrcode/src/utils/constant.dart';
+import 'package:fab_circular_menu/fab_circular_menu.dart';
 
-class ScanQRCodePage extends StatefulWidget {
+class TablePage extends StatefulWidget {
   @override
-  _ScanQRCodePageState createState() => _ScanQRCodePageState();
+  _TablePageState createState() => _TablePageState();
 }
 
-class _ScanQRCodePageState extends State<ScanQRCodePage> {
+class _TablePageState extends State<TablePage> with SingleTickerProviderStateMixin{
+
   @override
   Widget build(BuildContext context) {
+    var _width = MediaQuery.of(context).size.width / 2;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Constant.V_COLOR,
-        title: Text("ข้อมูลนิสิต"),
+        backgroundColor: Constant.G_COLOR,
+        title: Text("แสดงข้อมูลรายชื่อ"),
       ),
-      body: ListView(
-        children: <Widget>[
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: FittedBox(
-              child: DataTable(
-                columns: <DataColumn>[
-                  DataColumn(
-                    label: Text('ลำดับ'),
-                  ),
-                  DataColumn(
-                    label: Text('รหัสนิสิต'),
-                  ),
-                  DataColumn(
-                    label: Text('ชื่อ-นามสกุล'),
-                  ),
-                  DataColumn(
-                    label: Text('mid'),
-                  ),
-                  DataColumn(
-                    label: Text('final'),
-                  ),
-                  DataColumn(
-                    label: Text('คะแนนเก็บ ครั้ง1'),
-                  ),
-                  DataColumn(
-                    label: Text('คะแนนเก็บ ครั้ง2'),
-                  ),
-                  DataColumn(
-                    label: Text('คะแนนรวม'),
-                  ),
-                  DataColumn(
-                    label: Text('เกรด'),
-                  ),
-                ],
-                rows: <DataRow>[
-                  DataRow(cells: [
-                    DataCell(Text('1')),
-                    DataCell(Text('581463001')),
-                    DataCell(Text('มุกเอง')),
-                    DataCell(Text('40')),
-                    DataCell(Text('40')),
-                    DataCell(Text('10')),
-                    DataCell(Text('8')),
-                    DataCell(
-                      TextFormField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          contentPadding:
-                              EdgeInsets.fromLTRB(15.0, 13.0, 15.0, 13.0),
+      body: FabCircularMenu(
+      ringColor: Colors.grey,
+      options: <Widget>[
+        IconButton(icon: Icon(Icons.add), onPressed: () {}, iconSize: 48.0, color: Colors.white),
+        IconButton(icon: Icon(Icons.delete), onPressed: () {}, iconSize: 48.0, color: Colors.white),
+        IconButton(icon: Icon(Icons.update), onPressed: () {}, iconSize: 48.0, color: Colors.white),
+
+      ], 
+      child: Container(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            child: GridView.builder(
+        shrinkWrap: true,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2
+        ),
+        physics: BouncingScrollPhysics(),
+        itemCount: subject.length,
+        itemBuilder: (context, index){
+          return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: <Widget>[
+                      Card(
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
                         ),
-                        keyboardType: TextInputType.text,
+                        child: Container(
+                          width: _width,
+                          height: _width - 70,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                              /*child: FadeInImage(
+                                image: NetworkImage(_categoryImage[index]),
+                                fit: BoxFit.cover,
+                                placeholder:
+                                    AssetImage('assets/images/loading.gif'),
+                              ),*/
+                          ),
+                        ),
                       ),
-                    ),
-                    DataCell(Text('F')),
-                  ]),
-                ],
-              ),
+                      Text(subject[index].name,
+                          style: Theme.of(context).textTheme.body2),
+                      SizedBox(width: 5),
+                      Text('รหัสวิชา: '+subject[index].subjectcode.toString(),
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.bold
+                          )),
+                    ],
+                  ),
+                );
+              }
             ),
           ),
-          SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 150, left: 150),
-            child: Container(
-              height: 40,
-              width: double.infinity,
-              color: Constant.V_COLOR,
-              child: FlatButton(
-                child: Text(
-                  "บันทึก",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () {},
-              ),
-            ),
-          )
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_a_photo),
-        backgroundColor: Colors.blue,
-        onPressed: () {
-          print('hello');
-        },
-      ),
+        ),
+      )
+      )
     );
   }
 }
